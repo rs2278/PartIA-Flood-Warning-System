0215461016b28400fad6fab65800fbf4a292c086
rs2278 <157909757+rs2278@users.noreply.github.com>
2024-02-25 18:08:39 +0000

    I commit

---
+++ Task 2E.py
@@ -0,0 +1,68 @@
+import numpy as np
+from matplotlib import dates
+import matplotlib.pyplot as plt 
+from datetime import timedelta
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.station import MonitoringStation
+from floodsystem.stationdata import build_station_list, update_water_levels
+
+def plot_water_levels(station, dates=None, levels=None, dt=10, dotesting = False):
+
+   # Creating lists for dates and levels
+    dates_self = dates
+    levels_self = levels
+
+    # Fetching dates and levels for specific station    
+    # rough test to see if data is link as expected
+    if station.measure_id[0:4] != "http":
+        pass
+    else:
+        dates_self, levels_self = fetch_measure_levels(station.measure_id, dt=timedelta(days=dt))
+
+    # Plot
+    plt.plot(dates_self, levels_self)
+
+    # Add axis labels, rotate date labels and add plot title
+    plt.xlabel('date')
+    plt.ylabel('water level (m)')
+    plt.xticks(rotation=45)
+    plt.title(f"Station {station.name}")
+
+    # Plot of typical minimum and typical maximum, adding legend for clarity
+    plt.plot(dates_self, [station.typical_range[0]] * len(dates_self), label="Min")
+    plt.plot(dates_self, [station.typical_range[1]] * len(dates_self), label="Max")
+    plt.legend()
+
+    # Display plot
+    plt.tight_layout()
+
+    plt.show()
+
+    #for testing purposes, if dotest = True, return lists of values used in plotting function
+    if dotesting == True:
+        return dates_self, levels_self
+    else:
+        pass
+
+
+#from floodsystem.plot import plot_water_levels
+from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import update_water_levels
+from floodsystem.flood import stations_highest_rel_level
+
+
+def run():
+    # Initialization of variables that are needed afterwards
+    stations = build_station_list()
+    update_water_levels(stations)
+
+    #get list of stations you want to plot (top 5 highest relative water levels)
+    returned_list = stations_highest_rel_level(stations, 10)
+    #for each list, plot
+    for i in returned_list:
+        plot_water_levels(i)
+
+
+if __name__ == "__main__":
+    print("*** Task 2E: CUED Part IA Flood Warning System ***")
+    run()
--- Task 2F.py
+++ Task 2F.py
@@ -2,18 +2,46 @@ import numpy as np
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 
-def polyfit(dates, levels, p):
-    # Convert dates to float using date2num
-    x = mdates.date2num(dates)
+def plot_water_level_with_fit(station, dates1=None, levels=None, p=4, dt=2, dotesting = False):
+    # Creating empty lists for dates and levels
+    dates_self = dates1
+    levels_self = levels
 
-    # Find coefficients of best-fit polynomial of degree p
-    poly_coeff = np.polyfit(x, levels, p)
+    # Fetching dates and levels for specific station
+    # rough test to check data is correct
+    if station.measure_id[0:4] != "http":
+        print('ERROR the measure ID is invalid')
+        pass
+    else:
+        dates_self, levels_self = fetch_measure_levels(station.measure_id, dt=timedelta(days=dt))
 
-    # Convert coefficients into a polynomial that can be evaluated
-    poly = np.poly1d(poly_coeff)
+    #use polyfit and get fitted data
+    poly, d0 = polyfit(dates_self, levels_self, p)
+    x = dates.date2num(dates_self)
+    y = poly(x - d0)
 
-    # Determine the shift of the date (time) axis
-    d0 = dates[0]
+    #plot fitted and original data
+    plt.plot(dates_self, y, label="Power Fit")
+    plt.plot(dates_self, levels_self, label="Original")
 
-    return poly, d0
+    # Add axis labels, rotate date labels and add plot title
+    plt.xlabel('date')
+    plt.ylabel('water level (m)')
+    plt.xticks(rotation=45)
+    plt.title(f"Station {station.name}")
+
+    # Plot of typical minimum and typical maximum, adding legend for clarity
+    plt.plot(dates_self, [station.typical_range[0]] * len(dates_self), label="Min")
+    plt.plot(dates_self, [station.typical_range[1]] * len(dates_self), label="Max")
+    plt.legend()
+
+    # Display plot
+    plt.tight_layout()  # This makes sure plot does not cut off date labels
+
+    plt.show()
+
+
+    #for testing purposes if dotesting is True then return variables to be tested/asserted
+    if dotesting == True:
+        return x, y
 
